e692d4a2d749873a0f9745a466caf71b07e7285a
DurandoDev <nat.durando@gmail.com>
2024-02-14 09:56:59 +0100

    first commit

---
+++ src/main/java/com/medilabosolutions/backnotes/BackNotesApplication.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.backnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackNotesApplication.class, args);
+	}
+
+}
